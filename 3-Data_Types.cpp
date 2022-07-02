f6445430fbef4ff1fdb7ff0cdb9c425ddc95e65e
Luis Hayde <hayde.ldha@gmail.com>
2022-07-02 00:11:13 -0400

    The_Beginning

---
+++ 3-Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';                            //Just one character
+    string phrase = "Giraffe Academy";            //Differents characters
+    int age = 35;                                 //Just Integer numbers
+    float gpa1 = 2.3;                             //decimal numbers with a few decimals
+    double gpa2 = 2.3435;                         //decimal numbers with more decimals
+    bool isMale = true;                           //store true or false variable
+
+    cout << grade << endl;
+
+    return 0;
+}
